996ae028106a909e1a2c9cdf2aea0d8ab467664a
Marshall Brown <marshallendel@gmail.com>
2021-10-25 18:45:53 -0400

    UX improved html reset and form control

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 const App = {
+  init(selectors) {
+    this.dinos = [];
+    this.max = 0;
+    this.list = document.querySelector(selectors.listSelector);
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener("submit", this.addDino.bind(this));
+  },
 
-    init(selectors){ 
-        this.max = 0;
-        this.list = document.querySelector(selectors.listSelector)
-        document
-        .querySelector(selectors.formSelector)
-        .addEventListener("submit", this.addDino.bind(this))
-    },
+  addDino(e) {
+    e.preventDefault();
 
-    addDino(e){
-        e.preventDefault()
+    const dino = {
+      id: this.max + 1,
+      name: e.target.dinoName.value,
+    };
 
-        const dino = {
-            id: this.max + 1,
-            name:  e.target.dinoName.value,
+    this.renderListItem(dino);
+    this.dinos.push(dino.name);
+    this.max++;
 
-        }
+    e.target.reset();
+  },
 
-        this.renderListItem(dino)
-        this.max ++
-    },
-
-    renderListItem(dino){
-        const li = document.createElement("li")
-        li.innerText = dino.name
-        this.list.appendChild(li)
-    }
-}
+  renderListItem(dino) {
+    const li = document.createElement("li");
+    li.innerText = dino.name;
+    this.list.appendChild(li);
+  },
+};
 
 App.init({
-    formSelector: "#dino-form",
-    listSelector: "#dino-list"
-})
+  formSelector: "#dino-form",
+  listSelector: "#dino-list",
+});
